6d20c93c66d43d58def29725cf380f8218406dfa
georgiivanov12 <Kiromorskia@abv.bg>
2020-10-21 14:10:31 +0000

    .

---
+++ src/exercise4/ArrayInput .java
@@ -0,0 +1,22 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayInputOutput {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+
+
+
+
+        
+        
+        
+        
+        }
+}
+
+
